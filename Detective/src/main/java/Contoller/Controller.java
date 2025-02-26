96cd1b63a248bb5c0315b1bfe5a1f61e4f4281d5
vertolet1k <92737364+vertolet1k@users.noreply.github.com>
2025-02-26 11:59:34 +0300

    создали пакеты и поместили в них классы

---
+++ Detective/src/main/java/Contoller/Controller.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Contoller;
+
+/**
+ *
+ * @author vika
+ */
+public class Controller {
+    
+}
