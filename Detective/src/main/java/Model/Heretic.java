f2118a7d83e42eb0e5395f43f6aa1b7adfe56305
mariakuleshova <2maria.kuleshova@gmail.com>
2025-02-26 12:27:29 +0300

    Добавление методов в класс Heretic

--- Detective/src/main/java/Model/Heretic.java
+++ Detective/src/main/java/Model/Heretic.java
@@ -4,10 +4,65 @@
  */
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author vika
  */
 public class Heretic {
+    private String name;
+    private ArrayList <String> crimes;
+    private String lastPlace;
+    private Integer id;
+    private String dangerlevel;
+
+    public Heretic(String name, ArrayList<String> crimes, String lastPlace, Integer id, String dangerlevel) {
+        this.name = name;
+        this.crimes = crimes;
+        this.lastPlace = lastPlace;
+        this.id = id;
+        this.dangerlevel = dangerlevel;
+    }
+        
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getCrimes() {
+        return crimes;
+    }
+
+    public void setCrimes(ArrayList<String> crimes) {
+        this.crimes = crimes;
+    }
+
+    public String getLastPlace() {
+        return lastPlace;
+    }
+
+    public void setLastPlace(String lastPlace) {
+        this.lastPlace = lastPlace;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDangerlevel() {
+        return dangerlevel;
+    }
+
+    public void setDangerlevel(String dangerlevel) {
+        this.dangerlevel = dangerlevel;
+    } 
     
 }
