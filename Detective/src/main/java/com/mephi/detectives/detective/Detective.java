e0306ab13f5dcb1931afde7d30355d30ad731ce5
vertolet1k <92737364+vertolet1k@users.noreply.github.com>
2025-02-26 11:40:06 +0300

    создание проекта

---
+++ Detective/src/main/java/com/mephi/detectives/detective/Detective.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mephi.detectives.detective;
+
+/**
+ *
+ * @author vika
+ */
+public class Detective {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
